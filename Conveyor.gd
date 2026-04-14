95f3b1234e30926d8aa531a4390ecba5c70d3322
Mindbomber2 <79657039+Mindbomber2@users.noreply.github.com>
2026-04-15 00:42:12 +0200

    feat: images

--- Conveyor.gd
+++ Conveyor.gd
@@ -3,14 +3,18 @@ extends Area2D
 class_name Conveyor
 
 var color_rect:ColorRect
+var text_rect:TextureRect
 var state:int
 var direction:Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	color_rect = get_node("ColorRect")
+	text_rect = TextureRect.new()
+	add_child(text_rect)
 	direction = Vector2.ZERO
 	color_rect.set_color(Color.PURPLE)
+	text_rect.set_visible(true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,19 +27,36 @@ func _on_input_event(viewport, event, shape_idx):
 			change_state()
 
 func change_state():
-	state=(state+1)%3
+	state=(state+1)%5
 	if(state==0):
 		direction=Vector2.ZERO
-		color_rect.set_color(Color.PURPLE)
+		color_rect.set_visible(true)
+		text_rect.set_visible(false)
 		print("Null")
 	elif(state==1):
 		direction=Vector2.RIGHT
-		color_rect.set_color(Color.BLUE)
+		color_rect.set_visible(false)
+		text_rect.set_visible(true)
+		text_rect.texture = load("res://ConveyorRight.jpg")
 		print("Right")
 	elif (state==2):
 		direction=Vector2.DOWN
-		color_rect.set_color(Color.RED)
+		color_rect.set_visible(false)
+		text_rect.set_visible(true)
+		text_rect.texture = load("res://ConveyorDown.jpg")
 		print("Down")
+	elif (state==3):
+		direction=Vector2.LEFT
+		color_rect.set_visible(false)
+		text_rect.set_visible(true)
+		text_rect.texture = load("res://ConveyorLeft.jpg")
+		print("Left")
+	elif (state==4):
+		direction=Vector2.UP
+		color_rect.set_visible(false)
+		text_rect.set_visible(true)
+		text_rect.texture = load("res://ConveyorUp.jpg")
+		print("Up")
 
 
 func _on_body_entered(body: Node2D):
